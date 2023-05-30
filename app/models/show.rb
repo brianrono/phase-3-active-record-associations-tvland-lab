29de0dca773149b81994b1bcf3bc4109b6c0e5da
brianrono <brianrono16@gmail.com>
2023-05-30 22:31:29 +0300

    To finish

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network_id
+
+    def actor_list
+        self.actors.map(&:full_name)
+    end
+
+    def show_characters
+        Character.where(show_id: self.id)
+    end
 end
